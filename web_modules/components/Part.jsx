886ff56781359a0dc1bef77e84e43e8588167845
nmondon <nmondon@lefigaro.fr>
2015-06-02 14:40:02 +0200

    - React : first draft to display parts #4

--- web_modules/components/Part.jsx
+++ web_modules/components/Part.jsx
@@ -6,16 +6,21 @@ export default class Part extends React.Component {
 
     constructor(props){
         super(props)
+        this.state = {
+            click: 0
+        }
     }
 
     handleClick(){
-        alert('toto')
+        this.setState({
+            click: this.state.click+140
+        })
     }
 
     render(){
         return (
-            <div onClick={this.handleClick}>
-             tutdazefqrgsqrg
+            <div onClick={this.handleClick.bind(this)}>
+            {this.state.click}
             </div>
         )
     }
