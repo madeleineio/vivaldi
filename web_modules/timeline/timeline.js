a36b4c6934d4b18c1d38d773e115e8e1cbecf363
hllwd <nicolasmondon@gmail.com>
2015-06-16 22:49:08 +0200

    add timeline data

---
+++ web_modules/timeline/timeline.js
@@ -0,0 +1,19 @@
+'use strict'
+
+export default [{
+    text: 'Visualisation du concerto pour flute RV 439 de Vivaldi - scrollez pour continuer',
+    position: [-2000, 0, -2000],
+    lookAt: [0, 0, 0]
+},{
+    text: 'La visualisation est effectuée à partir d\'un fichier au format musicXML qui contient 7 partitions',
+    position: '',
+    lookAt: ''
+},{
+    text: 'La partition de la harpe contient des accords et différentes voix',
+    position: '',
+    lookAt: ''
+},{
+    text: 'Les notes les plus aigües sont en bleu, les plus graves en rouge',
+    position: '',
+    lookAt: ''
+}]
