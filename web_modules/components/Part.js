280d97ab17e7cb1a51936ce4fc26f2e1666ed848
nmondon <nmondon@lefigaro.fr>
2015-06-24 10:11:59 +0200

    - Remove React : score #3

---
+++ web_modules/components/Part.js
@@ -0,0 +1,9 @@
+'use strict'
+
+export default class Part {
+
+    constructor(){
+        console.log('part created')
+    }
+
+}
--- web_modules/main.jsx
+++ web_modules/main.jsx
@@ -1,25 +1,16 @@
-import React from 'react/addons.js'
 import $ from 'jquery'
 
 import getData from './services/getData.js'
-
 import { scene, camera, renderer, control } from './3d/setup.js'
+import Score from './components/Score.js'
 
-import Score3D from './components/Score3D.jsx'
-import Animate from './components/Animate.jsx'
 
 
 // first retrieve data from server
 $( () =>
         getData().then(data => {
-
-            React.render(
-                <div>
-                    <Score3D score={data.scorePartwise} />
-                    <Animate score={data.scorePartwise}/>
-                </div>,
-                document.querySelector('#react-container')
-            )
-
+            new Score({
+                score: data.scorePartwise
+            }).render()
         })
 )
