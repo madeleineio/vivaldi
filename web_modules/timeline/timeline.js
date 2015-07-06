89481ad2dae59b958ac8ef3d613aeca0395e094a
nmondon <nmondon@lefigaro.fr>
2015-07-06 13:20:12 +0200

    - Change text order

--- web_modules/timeline/timeline.js
+++ web_modules/timeline/timeline.js
@@ -3,25 +3,29 @@
 let timeline = [{
     lookAt: [0,2000,0],
     position: [-9000, -2000, 750],
-    text: ['Scrollez pour continuer']
+    text: [
+        'Cette visualisation, basée sur un fichier musicXML, représente le concerto pour flûte RV 439 de Vivaldi',
+        'La durée totale du concerto dépasse les 7 minutes',
+        'Scrollez pour continuer'
+    ]
 },{
     lookAt: [0,3000,0],
     position: [500, 4000, 10000],
     text: [
-        'Cette visualisation, basée sur un fichier musicXML, représente le concerto pour flûte RV 439 de Vivaldi',
-        'Chaque ligne correspond à une partition : Flûte, Harpe, Violon I, Violon II, Violon Alto, Violoncelle et Contrebasse',
+        'Chaque ligne correspond à une partition ; de gauche à droite:  Flûte, Harpe, Violon I, Violon II, Violon Alto, Violoncelle et Contrebasse',
+        'La partition de la harpe possède une plus grande complexité en raison des accords et voix qui font se chevaucher plusieurs notes sur un même temps',
         'Scrollez pour continuer']
 },{
     lookAt: [0,8000,0],
     position: [4000, 4000, -2000],
     text: [
-        'La partition de la harpe possède une plus grande complexité, en raison des accords et voix qui font se chevaucher plusieurs notes sur un même temps',
-        'Scrollez pour continuer']
+        'La hauteur des notes est représentée à la fois par la couleur (du bleu pour les aigus vers le rouge pour les graves), et par la profondeur dans l\'espace',
+        'Scrollez pour continuer'
+    ]
 },{
     lookAt: [0,9000,0],
     position: [2000, 9000, 0],
     text: [
-        'La hauteur des notes est représentée à la fois par la couleur (du bleu pour les aigus vers le rouge pour les graves), et par la profondeur dans l\'espace',
         'Scrollez pour continuer']
 },{
     lookAt: [0,14000,0],
